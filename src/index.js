da15b663e56256d22964e7aecbf0d61b0398fdfc
edensongjbs <edensongjbs@gmail.com>
2020-11-17 13:18:47 -0500

    finish documenting types

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@
  * @type {string}
  */
 const studentName = 'A String'
+
+/**
+ * An Array of Grades
+ * @type {Array<number>}
+ */
+
+const grades = [96, 81.4, 100]
+
+/**
+ * ToDo Object
+ * @type {{id: number|boolean, text: string}}
+ */
+const todo = {
+    id: false,
+    text: "A String"
+}
+
+const calculateTax = (amount, taxRate) => {
+    
+}
