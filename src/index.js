4e149b6dae86747b49731998cb1ca537a9284133
edensongjbs <edensongjbs@gmail.com>
2020-11-17 13:34:12 -0500

    doc function and objects and classes

--- src/index.js
+++ src/index.js
@@ -21,6 +21,56 @@ const todo = {
     text: "A String"
 }
 
+/**
+ * Calculate Amount with Tax
+ * @param {number} amount - total Amount pre-tax
+ * @param {number} taxRate - tax %
+ * @returns {string} - Total with a dollar sign
+ */
 const calculateTax = (amount, taxRate) => {
-    
+    return `$${amount + taxRate * amount}`
 }
+
+console.log(calculateTax(100, 0.1))
+
+/**
+ * A Student
+ * @typedef {Object} Student
+ * @property {number} id - Student ID
+ * @property {string} name - Student Name
+ * @property {string|number} [age] - Student ID [make it optional]
+ * @property {boolean} active - Is Student Active?
+ */
+
+ /**
+  * @type {Student}
+  */
+
+ const student = {
+     id: 1,
+     name: 'James',
+     age: 37,
+     active: true
+ }
+
+/**
+ * Class to create a person object
+ */
+
+ class Person {
+     /**
+      * 
+      * @param {Object} personInfo - Info About Person
+      */
+     constructor(personInfo) {
+        /**
+         * @property {string} name - Person's name
+         */
+        this.name = personInfo.name
+        /**
+         * @property {string|number} age Person's age
+         */
+        this.age = personInfo.age
+     }
+
+ }
